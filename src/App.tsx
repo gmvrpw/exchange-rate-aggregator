a5a7aac6df6e97edd94484a9f0cbd4750a9c04ba
gmvrpw <dev@gmvr.pw>
2023-05-08 12:32:08 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return <h1>Welcome to Exchange Rate Aggregator</h1>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { render } from "solid-js/web";
+import App from "./App";
+
+render(
+  () => <App />,
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  document.getElementById("root")!
+);
