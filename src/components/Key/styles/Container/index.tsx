134d5f87566e6146160df908c330dcba5274ff3c
gmvrpw <dev@gmvr.pw>
2023-05-23 14:47:53 +0300

    Add Key component

---
+++ src/components/Key/styles/Container/index.tsx
@@ -0,0 +1,13 @@
+import { styled } from "solid-styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-transform: lowercase;
+  font-size: 12px;
+  font-weight: 800;
+`;
+
+export default Container;
