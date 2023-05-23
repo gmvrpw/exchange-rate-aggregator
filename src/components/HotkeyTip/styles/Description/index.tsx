d4dede89d4fd84f6d21eb350456bc68a60d356f1
gmvrpw <dev@gmvr.pw>
2023-05-23 14:50:39 +0300

    Add HotkeyTip component

---
+++ src/components/HotkeyTip/styles/Description/index.tsx
@@ -0,0 +1,7 @@
+import { styled } from "solid-styled-components";
+
+const Description = styled.div`
+  font-size: 12px;
+`;
+
+export default Description;
---
+++ src/components/HotkeyTip/styles/Hotkeys/index.tsx
@@ -0,0 +1,8 @@
+import { styled } from "solid-styled-components";
+
+const Hotkeys = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export default Hotkeys;
